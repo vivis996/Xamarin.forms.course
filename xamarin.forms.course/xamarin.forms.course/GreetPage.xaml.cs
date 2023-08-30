04ded162b757673acd358d31026a435e84917947
Daniel Viveros <dviveros90@gmail.com>
2023-08-30 15:38:43 -0400

    Dealing with Device Differences

--- xamarin.forms.course/xamarin.forms.course/GreetPage.xaml.cs
+++ xamarin.forms.course/xamarin.forms.course/GreetPage.xaml.cs
@@ -9,6 +9,19 @@ namespace xamarin.forms.course
             InitializeComponent();
 
             slider.Value = 0.5;
+
+            switch (Device.RuntimePlatform)
+            {
+                case Device.iOS:
+                    Padding = new Thickness(0, 20, 0, 0);
+                    break;
+                case Device.Android:
+                    Padding = new Thickness(10, 20, 0, 0);
+                    break;
+            }
+
+            // Is deprecated
+            // Device.OnPlatform();
         }
     }
 }
