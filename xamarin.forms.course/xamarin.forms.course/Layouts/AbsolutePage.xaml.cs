697b720f3ce386ea82472af15cf0245fbae3439d
Daniel Viveros <dviveros90@gmail.com>
2023-08-30 20:55:13 -0400

    Absolute Layout in Code

--- xamarin.forms.course/xamarin.forms.course/Layouts/AbsolutePage.xaml.cs
+++ xamarin.forms.course/xamarin.forms.course/Layouts/AbsolutePage.xaml.cs
@@ -10,6 +10,16 @@ namespace xamarin.forms.course.Layouts
         public AbsolutePage()
         {
             InitializeComponent();
+
+            var absolute = new AbsoluteLayout();
+            Content = absolute;
+
+            var aquaBox = new BoxView { Color = Color.Aqua };
+            absolute.Children.Add(aquaBox, new Rectangle(0, 0, 1, 1),
+                                  AbsoluteLayoutFlags.All);
+
+            AbsoluteLayout.SetLayoutBounds(aquaBox, new Rectangle(0, 0, 1, 1));
+            AbsoluteLayout.SetLayoutFlags(aquaBox, AbsoluteLayoutFlags.HeightProportional);
         }
     }
 }
