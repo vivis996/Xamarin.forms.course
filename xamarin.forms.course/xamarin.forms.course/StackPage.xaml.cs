ee56c7e778f60f662ff478a0e981994aa07a88be
Daniel Viveros <dviveros90@gmail.com>
2023-08-30 20:06:51 -0400

    Stack Layout in code

--- xamarin.forms.course/xamarin.forms.course/StackPage.xaml.cs
+++ xamarin.forms.course/xamarin.forms.course/StackPage.xaml.cs
@@ -10,6 +10,16 @@ namespace xamarin.forms.course
 		public StackPage ()
 		{
 			InitializeComponent ();
+
+			var layout = new StackLayout
+			{
+				Spacing = 40,
+				Padding = new Thickness(0, 20, 0, 0),
+				Orientation = StackOrientation.Horizontal,
+			};
+			layout.Children.Add(new Label { Text = "Label 1" });
+
+			Content = layout;
 		}
 	}
 }
