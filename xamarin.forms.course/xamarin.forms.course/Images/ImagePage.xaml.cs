ca96ca478e44207303b92542f745dc5f8a039128
Daniel Viveros <dviveros90@gmail.com>
2023-09-01 16:38:32 -0400

    Embedded images

--- xamarin.forms.course/xamarin.forms.course/Images/ImagePage.xaml.cs
+++ xamarin.forms.course/xamarin.forms.course/Images/ImagePage.xaml.cs
@@ -10,15 +10,9 @@ namespace xamarin.forms.course.Images
         public ImagePage()
         {
             InitializeComponent();
-            const string imagePath = "https://wallpapers.com/images/featured/hd-a5u9zq0a0ymy2dug.jpg";
-            //this.image.Source = (UriImageSource)ImageSource.FromUri(new Uri(imagePath));
-            var imageSource = new UriImageSource { Uri = new Uri(imagePath) };
-            imageSource.CachingEnabled = false;
-            //imageSource.CacheValidity = TimeSpan.FromHours(1);
 
-            this.image.Source = imageSource;
-            //this.image.Aspect = Aspect.AspectFill;
-            //this.image.Source = imagePath;
+            const string imagePath = "xamarin.forms.course.files.images.background.jpg";
+            this.image.Source = ImageSource.FromResource(imagePath);
         }
     }
 }
