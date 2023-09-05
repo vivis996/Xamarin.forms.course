ba6b859ca063b9ad8aa7ff2bc057d1dd985afa93
Daniel Viveros <dviveros90@gmail.com>
2023-09-05 16:33:54 -0400

    Populating a Basic List

--- xamarin.forms.course/xamarin.forms.course/App.xaml.cs
+++ xamarin.forms.course/xamarin.forms.course/App.xaml.cs
@@ -10,7 +10,7 @@ namespace xamarin.forms.course
         {
             InitializeComponent();
 
-            MainPage = new Images.ImagePage();
+            MainPage = new Lists.ListPage();
         }
 
         protected override void OnStart ()
---
+++ xamarin.forms.course/xamarin.forms.course/Lists/ListPage.xaml.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Xamarin.Forms;
+
+namespace xamarin.forms.course.Lists
+{
+    public partial class ListPage : ContentPage
+    {
+        public ListPage()
+        {
+            InitializeComponent();
+
+            var names = new List<string>
+            {
+                "Daniel",
+                "John",
+                "Doe",
+            };
+
+            this.listView.ItemsSource = names;
+        }
+    }
+}
