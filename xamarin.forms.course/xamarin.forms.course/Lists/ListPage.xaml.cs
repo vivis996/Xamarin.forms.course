d4362b4e295e627891f69a6d876aa7a08093747d
Daniel Viveros <dviveros90@gmail.com>
2023-09-05 17:42:43 -0400

    Context Actions

--- xamarin.forms.course/xamarin.forms.course/Lists/ListPage.xaml.cs
+++ xamarin.forms.course/xamarin.forms.course/Lists/ListPage.xaml.cs
@@ -1,5 +1,5 @@
 ﻿using System;
-using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using xamarin.forms.course.Models;
 using Xamarin.Forms;
 
@@ -7,11 +7,13 @@ namespace xamarin.forms.course.Lists
 {
     public partial class ListPage : ContentPage
     {
+        private ObservableCollection<Contact> _contacts;
+
         public ListPage()
         {
             InitializeComponent();
 
-            this.listView.ItemsSource = new List<Contact>
+            this._contacts = new ObservableCollection<Contact>
             {
                 new Contact
                 {
@@ -31,6 +33,8 @@ namespace xamarin.forms.course.Lists
                     Status = "I'm new here",
                 },
             };
+
+            this.listView.ItemsSource = this._contacts;
         }
 
         void listView_ItemTapped(object sender, ItemTappedEventArgs e)
@@ -44,6 +48,19 @@ namespace xamarin.forms.course.Lists
             //var contact = e.SelectedItem as Contact;
             //DisplayAlert("Selected", contact.Name, "Ok");
             listView.SelectedItem = null;
+        }
+
+        void Call_Clicked(object sender, EventArgs e)
+        {
+            var menuItem = sender as MenuItem;
+            var contact = menuItem.CommandParameter as Contact;
+            DisplayAlert("Call", contact.Name, "Ok");
+        }
+
+        void Delete_Clicked(object sender, EventArgs e)
+        {
+            var contact = (sender as MenuItem).CommandParameter as Contact;
+            this._contacts.Remove(contact);
         }
     }
 }
