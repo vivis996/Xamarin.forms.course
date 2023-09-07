af0a330eced12f576ddc5880a9f053cc5fb38719
Daniel Viveros <dviveros90@gmail.com>
2023-09-07 19:22:23 -0400

    Bindable Properties

---
+++ xamarin.forms.course/xamarin.forms.course/Extensions/DateCell.xaml.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Xamarin.Forms;
+
+namespace xamarin.forms.course.Extensions
+{
+    public partial class DateCell : ViewCell
+    {
+        public static readonly BindableProperty LabelProperty =
+                               BindableProperty.Create(nameof(Label), typeof(string), typeof(DateCell));
+
+        public string Label
+        {
+            get
+            {
+                return (string)GetValue(LabelProperty);
+            }
+            set
+            {
+                SetValue(LabelProperty, value);
+            }
+        }
+
+        public DateCell()
+        {
+            InitializeComponent();
+            this.BindingContext = this;
+        }
+    }
+}
