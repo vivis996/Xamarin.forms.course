631d909dd7afdcf875e3be6e7e0cd587069d4ddf
Daniel Viveros <dviveros90@gmail.com>
2023-09-08 13:10:10 -0400

    Setting Up SQLite
    The classes are repeated if you want to use in Windows Forms (that's obsolete).
    I think it's not necessary to copy in both projects, maybe only in the main project.

---
+++ xamarin.forms.course/xamarin.forms.course.iOS/DataAccess/Persistence/SQLiteDb.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.IO;
+using SQLite;
+using xamarin.forms.course.DataAccess.Persistence;
+using xamarin.forms.course.iOS.DataAccess.Persistence;
+using Xamarin.Forms;
+
+[assembly: Dependency(typeof(SQLiteDb))]
+namespace xamarin.forms.course.iOS.DataAccess.Persistence
+{
+    public class SQLiteDb : ISQLiteDb
+    {
+        public SQLiteAsyncConnection GetConnection()
+        {
+            const string DbName = "MySQLite.db3";
+            var documentsPath = Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments);
+            var path = Path.Combine(documentsPath, DbName);
+
+            return new SQLiteAsyncConnection(path);
+        }
+    }
+}
