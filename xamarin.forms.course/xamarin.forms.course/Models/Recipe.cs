19d44112a8a054fd7c893289496aee463300c337
Daniel Viveros <dviveros90@gmail.com>
2023-09-08 13:46:58 -0400

    INotifyPropertyChanged Interface

--- xamarin.forms.course/xamarin.forms.course/Models/Recipe.cs
+++ xamarin.forms.course/xamarin.forms.course/Models/Recipe.cs
@@ -1,17 +1,39 @@
-﻿using SQLite;
+﻿using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using SQLite;
 
 namespace xamarin.forms.course.Models
 {
     // To change the name table
     [Table("Recipes")]
-    public class Recipe
+    public class Recipe : INotifyPropertyChanged
     {
+        public event PropertyChangedEventHandler PropertyChanged;
+
         // Add [Column("RecipeId")] If I want to change
         // the name of the column
         [PrimaryKey, AutoIncrement]
         public int Id { get; set; }
 
+        private string _name;
         [MaxLength(255)]
-        public string Name { get; set; }
+        public string Name
+        {
+            get => this._name;
+            set
+            {
+                if (this._name == value)
+                    return;
+
+                this._name = value;
+
+                this.OnPropertyChanged();
+            }
+        }
+
+        private void OnPropertyChanged([CallerMemberName] string propertyName = "")
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
     }
 }
