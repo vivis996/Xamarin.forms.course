f073dc73604fb7ad6a0c28b6554b3eb2094b1367
Daniel Viveros <dviveros90@gmail.com>
2023-09-10 16:41:10 -0400

    Extracting a View Model

---
+++ xamarin.forms.course/xamarin.forms.course/MVVM/ViewModels/PlayListsViewModel.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections.ObjectModel;
+using xamarin.forms.course.MMVM.Models;
+
+namespace xamarin.forms.course.MVVM.ViewModels
+{
+    public class PlayListsViewModel
+    {
+        public ObservableCollection<Playlist> Playlists { get; private set; } =
+                     new ObservableCollection<Playlist>();
+
+        public Playlist SelectedPlaylist { get; set; }
+
+        public void AddPlaylist()
+        {
+            var newPlaylist = "Playlist " + (this.Playlists.Count + 1);
+
+            this.Playlists.Add(new Playlist { Title = newPlaylist });
+        }
+
+        public void SelectPlayList(Playlist playlist)
+        {
+            if (playlist is null)
+                return;
+
+            playlist.IsFavorite = !playlist.IsFavorite;
+
+            this.SelectedPlaylist = null;
+            //await Navigation.PushAsync (new PlaylistDetailPage(playlist));
+        }
+    }
+}
--- xamarin.forms.course/xamarin.forms.course/MVVM/Views/PlaylistsPage.xaml.cs
+++ xamarin.forms.course/xamarin.forms.course/MVVM/Views/PlaylistsPage.xaml.cs
@@ -1,47 +1,32 @@
 ﻿using System;
-using System.Collections.ObjectModel;
 using xamarin.forms.course.MMVM.Models;
+using xamarin.forms.course.MVVM.ViewModels;
 using Xamarin.Forms;
 
 namespace xamarin.forms.course.MMVM.Views
 {
     public partial class PlaylistsPage : ContentPage
     {
-        private readonly ObservableCollection<Playlist> _playlists =
-                     new ObservableCollection<Playlist>();
-
         public PlaylistsPage()
         {
+            this.BindingContext = new PlayListsViewModel();
             InitializeComponent();
         }
 
         protected override void OnAppearing()
         {
-            playlistsListView.ItemsSource = this._playlists;
-
             base.OnAppearing();
         }
 
         void OnAddPlaylist(object sender, EventArgs e)
         {
-            var newPlaylist = "Playlist " + (this._playlists.Count + 1);
-
-            this._playlists.Add(new Playlist { Title = newPlaylist });
-
-            this.Title = $"{this._playlists.Count} Playlists";
+            (this.BindingContext as PlayListsViewModel).AddPlaylist();
         }
 
         void OnPlaylistSelected(object sender, SelectedItemChangedEventArgs e)
         {
-            if (e.SelectedItem is null)
-                return;
-
             var playlist = e.SelectedItem as Playlist;
-            playlist.IsFavorite = !playlist.IsFavorite;
-
-            playlistsListView.SelectedItem = null;
-
-            //await Navigation.PushAsync (new PlaylistDetailPage(playlist));
+            (this.BindingContext as PlayListsViewModel).SelectPlayList(playlist);
         }
     }
 }
